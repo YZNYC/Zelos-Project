ec92ef7433b24f6216a1bcf87056079ab77be183
YZNYC <nycolasdelimaleandro@gmai]l.com>
2025-08-07 14:55:09 -0300

    authMiddleware

--- Zelos-main/backend/middlewares/authMiddleware.js
+++ Zelos-main/backend/middlewares/authMiddleware.js
@@ -1,22 +1,29 @@
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../config/jwt.js'; // Importar a chave secreta
+import { JWT_SECRET } from '../config/jwt.js';
 
-const authMiddleware = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+const AuthMiddleware = (...tiposPermitidos) => {
+  return (req, res, next) => {
+    const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    return res.status(401).json({ mensagem: 'Não autorizado: Token não fornecido' });
-  }
+    if (!authHeader) {
+      return res.status(401).json({ mensagem: 'Token não fornecido' });
+    }
 
-  const [ , token] = authHeader.split(' ');
+    const token = authHeader.split(' ')[1];
 
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    req.usuarioId = decoded.id;
-    next();
-  } catch (error) {
-    return res.status(403).json({ mensagem: 'Não autorizado: Token inválido' });
-  }
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET);
+      req.usuario = decoded;
+
+      if (!tiposPermitidos.includes(decoded.tipo)) {
+        return res.status(403).json({ mensagem: 'Acesso negado: permissão insuficiente' });
+      }
+
+      next();
+    } catch (error) {
+      return res.status(401).json({ mensagem: 'Token inválido ou expirado' });
+    }
+  };
 };
 
-export default authMiddleware;
+export default AuthMiddleware;
