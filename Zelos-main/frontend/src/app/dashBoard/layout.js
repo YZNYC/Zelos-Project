2c9f80176f3ab1d6d8fbf21c82524038c06fb0d5
YZNYC <nycolasdelimaleandro@gmail.com>
2025-08-14 11:20:32 -0300

    Dashboard

--- Zelos-main/frontend/src/app/dashBoard/layout.js
+++ Zelos-main/frontend/src/app/dashBoard/layout.js
@@ -1,13 +1,13 @@
 'use client';
 
-import Sidebar from '../Components/Sidebar/Sidebar';
-import Header from '../Components/Header/Header';
+import Sidebar from "../Components/Sidebar/Sidebar";
+import Header from "../Components/Header/Header";
 
 
 export default function DashboardLayout({ children }) {
   return (
     <div className="flex h-screen">
-         <Header />
+         <Header/>
       <Sidebar/>
       <main className="flex-1 overflow-hidden">
         {children}
--- Zelos-main/frontend/src/app/dashBoard/page.jsx
+++ Zelos-main/frontend/src/app/dashBoard/page.jsx
@@ -1,5 +1,17 @@
 'use client'
 import { useState, useEffect } from "react";
+import DashboardCard from '../Components/DashboardCard/Card';
+import { TbNumber } from "react-icons/tb";
+import {  PiPlusCircleBold, PiPencilSimpleBold, PiTrashBold, PiListChecksBold } from "react-icons/pi";
+
+const cardBaseStyle = "bg-gray-100 border border-slate-700 rounded-xl p-5  transition-all duration-200 flex flex-col min-h-[180px] sm:min-h-[200px]";
+const cardActionStyle = "cursor-pointer hover:border-sky-500 active:bg-white";
+const cardIconColor = "text-white";
+const cardIconSize = 30;
+const cardTitleStyle = "font-semibold text-slate-100 text-base sm:text-lg mb-1 mt-2";
+const cardDescriptionStyle = "text-slate-400 text-xs sm:text-sm flex-grow";
+const cardValueStyle = `font-bold text-2xl sm:text-3xl mb-1 ${cardIconColor}`;
+
 
 export default function dashBoard() {
 
@@ -8,57 +20,61 @@ export default function dashBoard() {
 
       <>
         <main
-          className="h-screen bg-cover bg-center bg-no-repeat overflow-hidden px-4 md:px-10 flex items-center justify-center"
-          style={{ backgroundImage: `url('/BG-dash.jpg')` }}
+          className="h-screen bg-cover bg-center bg-no-repeat overflow-hidden px-10 md:px-10 ml-10 flex items-center justify-center"
+          style={{ backgroundColor: "#D3D3D3"}}
         >
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-5 md:gap-6 ">
+                <DashboardCard
+                  cardStyle={cardBaseStyle} iconColor={cardIconColor} titleStyle={cardTitleStyle} descriptionStyle={cardDescriptionStyle} valueStyle={cardValueStyle}
+                  icon={<TbNumber size={cardIconSize} />}
+                  title="Total de Admins"
+                  description="Número atual de administradores ativos no sistema."
+                      value={"nada"}
+                />
+                <DashboardCard
+                  cardStyle={`${cardBaseStyle} ${cardActionStyle}`} iconColor={cardIconColor} titleStyle={cardTitleStyle} descriptionStyle={cardDescriptionStyle}
+                  icon={<PiPlusCircleBold size={cardIconSize} />}
+                  title="Novo Administrador"
+                  description="Clique para adicionar um novo perfil de administrador."
+                  onClick={() => setShowModalCadastroAdmin(true)}
+                  action
+                />
+                <DashboardCard
+                  cardStyle={`${cardBaseStyle} ${cardActionStyle}`} iconColor={cardIconColor} titleStyle={cardTitleStyle} descriptionStyle={cardDescriptionStyle}
+                  icon={<PiListChecksBold size={cardIconSize} />}
+                  title="Listar Administradores"
+                  description="Acesse o relatório completo de administradores."
+                  onClick={() => setShowModalRelatorioAdmin(true)}
+                  action
+                />
+                <DashboardCard
+                  cardStyle={`${cardBaseStyle} ${cardActionStyle}`} iconColor={cardIconColor} titleStyle={cardTitleStyle} descriptionStyle={cardDescriptionStyle}
+                  icon={<PiPencilSimpleBold size={cardIconSize} />}
+                  title="Editar Admin"
+                  description="Modifique informações de um administrador existente."
+                  onClick={() => setShowModalUpdateAdmin(true)}
+                  action
+                />
+                <DashboardCard
+                  cardStyle={`${cardBaseStyle} ${cardActionStyle} hover:border-red-500 group`} iconColor="text-red-400 group-hover:text-red-500" titleStyle={cardTitleStyle} descriptionStyle={cardDescriptionStyle}
+                  icon={<PiTrashBold size={cardIconSize} />}
+                  title="Excluir Admin"
+                  description="Remova um administrador do sistema (ação irreversível)."
+                  onClick={() => setShowModalExcluirAdmin(true)}
+                  action
+                />
+                <DashboardCard
+                  cardStyle={`${cardBaseStyle} ${cardActionStyle} hover:border-red-500 group`} iconColor="text-red-400 group-hover:text-red-500" titleStyle={cardTitleStyle} descriptionStyle={cardDescriptionStyle}
+                  icon={<PiTrashBold size={cardIconSize} />}
+                  title="Historico"
+                  description="Visualize o historico de alterações"
+                  onClick={() => setShowModalHistorico(true)}
+                  action
+                />
 
-          {/* Versão desktop e mobile: visível fora do md */}
-          <section className="flex-1 flex flex-col items-center justify-center text-center md:hidden px-6 sm:px-8">
-            <h1 className="text-2xl text-white font-bold pt-10 max-w-full break-words">
-              Bem-vindo ao V-MAP
-            </h1>
-            <h2 className="mt-6 text-sm text-white max-w-xs sm:max-w-md px- sm:px-0">
-              O V-MAP é um sistema feito para as escolas de Monte Azul Paulista que ajuda a
-              rastrear os ônibus escolares em tempo real. Com ele, a escola, os alunos e os
-              responsáveis conseguem ver onde o ônibus está, acompanhar o trajeto e ter mais
-              segurança no dia a dia. Tudo isso de forma simples, rápida e confiável.
-            </h2>
-            <div className="pt-10">
-            </div>
-          </section>
+                </div>
 
-          {/* Versão tablet: só texto centralizado, visível só no md */}
-          <section className="hidden md:flex lg:hidden flex-1 flex-col items-center justify-center text-center h-screen px-6">
-            <h1 className="text-4xl font-bold text-white">
-              Bem-vindo ao V-MAP
-            </h1>
-            <h2 className="mt-6 text-lg max-w-xl text-white">
-              O V-MAP é um sistema feito para as escolas de Monte Azul Paulista que ajuda a
-              rastrear os ônibus escolares em tempo real. Com ele, a escola, os alunos e os
-              responsáveis conseguem ver onde o ônibus está, acompanhar o trajeto e ter mais
-              segurança no dia a dia. Tudo isso de forma simples, rápida e confiável.
-            </h2>
-          </section>
 
-          {/* Versão desktop: texto alinhado à esquerda e imagem visível no lg+ */}
-          <section className="hidden lg:flex flex-1 flex-row items-center justify-between px-10 gap-10">
-            {/* Div do texto à esquerda */}
-            <div className="flex-1 flex flex-col items-start justify-center max-w-xl">
-              <h1 className="text-5xl mb-10 font-bold text-white">
-                Bem-vindo ao V-MAP
-              </h1>
-              <h2 className="mt-6 text-2xl text-white">
-                O V-MAP é um sistema feito para as escolas de Monte Azul Paulista que ajuda a
-                rastrear os ônibus escolares em tempo real. Com ele, a escola, os alunos e os
-                responsáveis conseguem ver onde o ônibus está, acompanhar o trajeto e ter mais
-                segurança no dia a dia. Tudo isso de forma simples, rápida e confiável.
-              </h2>
-            </div>
-
-            {/* Div da imagem à direita */}
-            <div className="flex-1 flex items-center justify-center pt-10">
-            </div>
-          </section>
         </main>
       </>
   );
