da719288d5a56bf792a935342009325a34ed3c9d
YZNYC <nycolasleandro@outlook.com>
2025-09-22 10:15:00 -0300

    update | relatorios

--- Zelos-main/frontend/src/app/Relatorios/page.jsx
+++ Zelos-main/frontend/src/app/Relatorios/page.jsx
@@ -4,7 +4,7 @@ import CardRelatorio from "../Components/dashboard/CardRelatorio";
 export default function Relatorio() {
   return (
     <div className="p-6">
-      <h1 className="text-5xl font-bold mt-30 ml-6">Relatórios</h1>
+      <h1 className="text-5xl font-bold mt-20 ml-6">Relatórios</h1>
       <div className="p-6 mt-5">
         <CardRelatorio />
       </div>
